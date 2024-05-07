d5591cadf2b6e75f807b9ef7d2b60116a8710b31
km <msn.moise@hotmail.fr>
2024-05-07 19:08:42 +0200

    mon CV minimaliste

---
+++ src/pages/content.js
@@ -0,0 +1,293 @@
+// Personal Information --> #hero section
+export const name = "K - M 🤷"
+export const designation = "Infirmier en USIC"
+export const location = "Paris"
+export const pronouns = "🇲🇫"
+export const website = "https://chat.openai.com/"
+
+// A propos de moi --> About me section
+export const about = " En tant qu'infirmier expérimenté avec plus de dix années de pratique, j'ai eu le privilège de travailler dans divers environnements de santé, acquérant ainsi une expertise solide en soins infirmiers.  Cette noble profession m'a enseigné des leçons inestimables sur la vie, la patience, la motivation, les relations humaines, l 'espoir, la prise d'initiative, l'endurance et le travail d'équipe.  Cette expérience enrichissante m'a permis de développer un profond respect pour le dévouement et la compassion nécessaires pour offrir des soins de haute qualité à chaque patient.  La complexité et la sensibilité des cas rencontrés en unité de soins intensifs cardiologiques me fascinent particulièrement.  Je suis convaincu que mon expérience antérieure, combinée à ma capacité à gérer efficacement les situations médicales critiques, me permettra de contribuer de manière significative à l équipe et de fournir des soins exceptionnels aux patients nécessitant une surveillance étroite et une attention spécialisée,  🤷  ."
+
+// Work Experience --> #work section
+export const work = [
+	{
+		from: "...",
+		to: "...",
+		role: "Lieu des stages et formation",
+		org: "Lien",
+		org_url: "https://chu-mondor.aphp.fr/",
+		location: "Créteil",
+		skills: "Hôpital Henri Mondor",
+	},
+	{
+		from: "...",
+		to: "...",
+		role: "Services des stages effctués durant la formation :",
+		org: "Lien",
+		org_url: "https://chu-mondor.aphp.fr/",
+		location: "Gériatrie, Psychiatrie, Médecin interne, Les urgences, Neurochirurgie, Oncologie, Hématologie,  salle de Réveil SSPI",
+		skills: "Hôpital Henri Mondor",
+	},
+	{
+		from: "Lieu de travail : 2006",
+		to: "2015",
+		role: "Clinique la Dhuys",
+		org: "Lien",
+		org_url: "https://quahog.gov/safety",
+		location: "Bagnolet",
+		skills: "Service USIC et cardiologie .",
+	},
+	{
+		from: "Lieu de travail : 2016",
+		to: "2018",
+		role: "Clinique Floréal",
+		org: "Lien",
+		org_url: "https://cliniquefloreal.com/fr/",
+		location: "Bagnolet",
+		skills: "Service USIC .",
+	},
+	{
+		from: "Lieu de travail : 2013",
+		to: "présent",
+		role: "Hopital la Roseraie",
+		org: "Lien",
+		org_url: "https://www.hopitaleuropeendeparis.fr/home",
+		location: "Aubervilliers",
+		skills: "Service USIC ."
+	},
+	{
+		from: "Lieu de travail : 2014",
+		to: "2016",
+		role: "Hopital privé paul d'égine",
+		org: "Lien",
+		org_url: "https://hopital-prive-paul-d-egine-champigny-sur-marne.ramsaysante.fr/",
+		location: "Champigny - sur - Marne",
+		skills: "Service USIC ,service de chirurgie général et orthopédique, service d'oncologie"
+	},
+	{
+		from: "Lieu de travail : 2021",
+		to: "2021",
+		role: "Hopital Paris Saint - joseph",
+		org: "Lien",
+		org_url: "https://www.hpsj.fr/",
+		location: "Paris 14",
+		skills: "Service USIC"
+	},
+	{
+		from: "Lieu de travail :  2018",
+		to: "2018",
+		role: "Clinique Ambroise Paré",
+		org: "Lien",
+		org_url: "https://www.ambroisepare.fr/",
+		location: "Neuilly - sur - Seine",
+		skills: "Service USIC"
+	}
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		from: "A définir",
+		to: "Présent",
+		title: "Master dans le management",
+		description: "Préparation master IPA ou management : solidarité internationale et action sociale",
+		//url: "https://projectgriffin.com",
+		tech: "A définir",
+	},
+	{
+		from: "A définir",
+		to: "présent",
+		title: "Diplôme d'université Sorbonne Data Analytics",
+		description: "Programmation et statistiques",
+		//url: "https://safefamilyquahog.com",
+		tech: "A définir ",
+	},
+	{
+		from: "A définir",
+		to: "Présent",
+		title: "Concours IADE",
+		description: "A définir",
+		//url: "https://roadsafetyquahog.com",
+		tech: "A définir",
+	}
+]
+
+// Education --> #education section
+export const education = [
+	{
+		from: "1994",
+		to: "1995",
+		institute: "Baccalaureat",
+		course: "Baccalauréat candidat scolaire",
+		location: "Algérie"
+	},
+	{
+		from: "1995",
+		to: "1996",
+		institute: "Baccalaureat",
+		course: "Baccalauréat candidat Libre",
+		location: "Algérie"
+	},
+	{
+		from: "...",
+		to: "...",
+		institute: "Diplôme d'etat",
+		course: "Infirmier diplômé d'état, IDE",
+		location: "Créteil, France"
+	},
+	{
+		from: "2005",
+		to: "2006",
+		institute: "Diplome universitaire",
+		course: "Prise en charge de la douleur",
+		location: "Université Versailles Saint-Quentin-en-Yvelines (UVSQ) - Université Paris Saclay , France"
+	},
+	{
+		from: "...",
+		to: "...",
+		institute: "Diplome universitaire",
+		course: "Transport Aérien et Rapatriément sanitaire",
+		location: "UFR - Saint - Antoine , Paris , France"
+	},
+	{
+		from: "...",
+		to: "...",
+		institute: "Diplome universitaire",
+		course: "Téchnicien de recherche clinique",
+		location: "UFR - Créteil , Paris , France"
+	},
+	{
+		from: "2020",
+		to: "2021",
+		institute: "Certification",
+		course: "Développeur web et web mobile frontend",
+		location: "Paris , France"
+	},
+	{
+		from: "2022",
+		to: "2023",
+		institute: "Diplome d'état",
+		course: "Développeur web et web mobile fullstack JavaScript",
+		location: "Paris , France"
+	}
+]
+
+// Contact --> #contact section
+export const contact = [
+	{
+		source_name: "msn.moise@hotmail.fr",
+		source: "#",
+		tags:"msn.moise@hotmail.fr",
+		title: "address e-mail :",
+		//location: "Paris , France",
+		source:"#"
+
+	},
+	{
+		source_name: "06XXXXXXXX",
+		source: "#",
+		tags:"Si vous tomber sur la messagerie n'hésiter pas à me laisser  un message, je vous recontacterai . ",
+		title: "Téléphone :",
+		location: "Paris , France",
+		source:"#"
+	}
+,
+	{
+		source_name: "LinkedIn",
+		tags:"compte LinkedIn est un réseau social professionnel en ligne",
+		title: "linkedin",
+		location: "Paris , France",
+		source: "https://www.linkedin.com/",
+	},
+]
+
+// Certifications --> #certificate section
+
+export const certificates = [
+	{
+		issued: "1994",
+		title: "Baccalaureat Scientifique",
+		org: "Enseignment secondaire",
+		//url: "https://certi.com/url",
+		description: "enseignement secondaire , Pays, Algérie",
+	},
+	{
+		issued: "1995",
+		title: "2 ème Baccalaureat scientifique",
+		org: "Enseignement secondaire",
+		//url: "https://certi.com/url",
+		description: "enseignement secondaire, Pays, Algérie",
+	},
+	{
+		issued: "Diplôme d'état : IDE",
+		title: "Bac + 3",
+		org: "Enseignement universitaire",
+		//url: "https://certi.com/url",
+		description: "Domaine de la santé, Pays , France",
+	},
+	{
+		issued: "Diplôme d'état : DEV",
+		title: "Bac + 3",
+		org: "Enseignement universitaire",
+		//url: "https://certi.com/url",
+		description: "Domaine du développement web, Pays , France",
+	},
+	{
+		issued: "Diplôme d'état : AM en AR",
+		title: "Bac + 3",
+		org: "Enseignement universitaire",
+		//url: "https://certi.com/url",
+		description: "Domaine de la santé",
+	},
+	{
+		issued: "Diplôme Universitaire et Inter-Universitaire",
+		title: "Post-graduate",
+		org: "Enseignement universitaire",
+		//url: "https://certi.com/url",
+		description: "Domaine de la santé, pays, France .",
+	}
+] 
+
+// Blogs --> # Languages section
+export const blogs = [
+	{
+		date: "Français",
+		//url: "...",
+		title: "⭐⭐⭐⭐⭐",
+		description: "Parler depuis l'enfance",
+		//publisher: "Bilingue",
+	},
+	{
+		date: "Arabic",
+		//url: "https://medium.com/",
+		title: "⭐⭐⭐⭐⭐",
+		description: "Langue maternelle",
+		//publisher: "",
+	},
+	{
+		date: "Anglais",
+		//url: "https://medium.com/",
+		title: "⭐⭐",
+		description: "Langue étrangère",
+		//publisher: "Bilingue",
+	}
+]
+
+// Achievements --> # Interests section
+export const achievements = [
+	{
+		title: "Musique Douce, Rock, Jazz, Chaabi, ...",
+		year: "Musique.",
+		decription: "Autres....",
+	},
+	{
+		title: "Téchnologies du web",
+		year: "Programmation",
+		decription: "Développement backend et frontend"
+	},
+	{
+		title: "Foot, Natation, Cyclisme, ...",
+		year: "Sports.",
+		decription: "Club réal madrid ⚽"
+	}
+]
